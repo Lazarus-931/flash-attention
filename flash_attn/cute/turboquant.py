6a19114805f3f8aa082f9eae2666c86966f87110
Alazar Manakelew <alazermanakelewb@gmail.com>
2026-03-26 18:48:53 -0700

    Arithmetic dequant: odd polynomial eval, no lookup table

--- flash_attn/cute/turboquant.py
+++ flash_attn/cute/turboquant.py
@@ -14,6 +14,20 @@ CODEBOOK = {
          1.25623664,  1.61805216,  2.06902289,  2.73259458],
 }
 
+ODD_POLY_COEFFS = {
+    1: [1.59576912],
+    2: [0.05069251, 0.89288693],
+    3: [4.07666333e-04, 5.24606083e-03, 4.89310189e-01],
+    4: [4.11131823e-07, -9.31039455e-06, 1.14310329e-03, 2.56189071e-01],
+}
+
+ODD_POLY_CENTER = {
+    1: 0.5,
+    2: 1.5,
+    3: 3.5,
+    4: 7.5,
+}
+
 
 class TurboQuant:
     def __init__(self, num_bits: int, dtype):
@@ -24,15 +38,22 @@ class TurboQuant:
         self.elems_per_int32 = 32 // num_bits
         self.mask = (1 << num_bits) - 1
         self.num_entries = len(self.entries)
+        self.poly_coeffs = ODD_POLY_COEFFS[num_bits]
+        self.center = ODD_POLY_CENTER[num_bits]
 
     @cute.jit
     def dequantize(self, packed_data: cute.Tensor, output: cute.Tensor, num_int32s: Int32):
-        cb = cute.make_rmem_tensor((self.num_entries,), self.dtype)
-        for c in cutlass.range_constexpr(self.num_entries):
-            cb[c] = self.dtype(self.entries[c])
+        center = self.dtype(self.center)
+        coeffs = [self.dtype(c) for c in self.poly_coeffs]
+        num_coeffs = len(self.poly_coeffs)
 
         for i in cutlass.range_constexpr(num_int32s):
             packed_val = packed_data[i]
             for j in cutlass.range_constexpr(self.elems_per_int32):
                 code = (packed_val >> (j * self.num_bits)) & self.mask
-                output[i * self.elems_per_int32 + j] = cb[code]
+                t = self.dtype(code) - center
+                t2 = t * t
+                val = coeffs[0]
+                for k in cutlass.range_constexpr(1, num_coeffs):
+                    val = val * t2 + coeffs[k]
+                output[i * self.elems_per_int32 + j] = val * t
